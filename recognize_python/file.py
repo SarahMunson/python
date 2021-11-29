6e756ed4717aa2bd819cbc11b654c30c71894d68
John Doe <s.maeflower@gmail.com>
2021-11-29 13:19:22 -0600

    initial commit

---
+++ recognize_python/file.py
@@ -0,0 +1,58 @@
+#variable declaration:
+my_name = "Sarah"
+
+#log statement:
+print(my_name)
+
+#type check
+print(type(my_name))
+
+#length check
+print(len(my_name))
+
+#comment -single line
+'''comment 
+multi-line'''
+
+#datatypes:
+# 1. primitive == string, number, Boolean
+
+# 2. composite == a. lists, b. tuples, c. dictionary
+
+    '''a. lists
+        -initialize
+        - access value
+        - change value
+        - add value
+        - delete value'''
+
+    '''b. Tuples
+        - initialize
+        - access value
+        - change value
+        - add value
+        - delete value'''
+
+    '''c. Dictionary
+        - initialize
+        - access value
+        - change value
+        - add value
+        - delete value'''
+
+#Conditional == if, else if, else
+
+#for loop == start, stop, increment, break, continue, sequence
+
+#while loop == start, stop, increment
+
+#function == parameter, argument, return
+
+#*Bonus: Errors == 
+#NameError: name <variable name> is not defined
+#TypeError: 'tuple' object does not support item assignment
+#KeyError: 'favorite_team'
+#IndexError: list index out of range
+#IndentationError: list index out of range
+#AttributeError: 'tupe' object has no attribute 'pop'
+#AttributeError: 'tuple' object has no attribute 'append'
