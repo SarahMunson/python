647216d78d881b9acc45d47f5efc07dff27cef39
John Doe <s.maeflower@gmail.com>
2021-12-05 12:53:53 -0600

    numbers game complete

--- w1d5/great_number_game/server.py
+++ w1d5/great_number_game/server.py
@@ -1,32 +1,24 @@
 from flask import Flask, render_template, request, redirect, session
+import random
 
 app = Flask(__name__)
 app.secret_key = 'keep it safe'
 
 @app.route('/')
 def root():
-    session['random_number'] = 55
-    if int(session['guess']) < session['random_number']:
-        print("Too Low")
-    elif int(session['guess']) > session['random_number']:
-        print("Too high")
-    else:
-        print("You guessed it!")
+    if "num" not in session:
+        session['num'] = random.randint(1, 100)
     return render_template('index.html')
 
 @app.route('/guess', methods=['POST'])
 def guess():
     session['guess'] = int(request.form['guess'])
-    session['random_number'] = 55
-    if int(session['guess']) < session['random_number']:
-        print("Too Low")
-    elif int(session['guess']) > session['random_number']:
-        print("Too high")
-    else:
-        print("You guessed it!")
     return redirect('/')
 
-
+@app.route('/playagain')
+def play_again():
+    session.clear()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug = True)
